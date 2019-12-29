43bb03d491ed1b42ee943db666fb2985a6093f41
YWQBX <57152644+YWQBX@users.noreply.github.com>
2019-12-30 01:03:35 +0900

    web

--- js/LendShit.js
+++
@@ -1,6 +0,0 @@
-function LendShit(){
-    if(){
-
-    }
-
-}
---
+++ js/self.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	var nice = $("html").niceScroll();
+    $("#colorlib-aside").niceScroll({cursorborder:"",cursorcolor:"#FFFFFF",boxzoom:true});
+
+  });
